1090294b0b94dff2b89068b016a8e85f18d72e7e
JeroenDerks <jeroen.derks@lau.do>
2022-05-16 21:44:50 +0200

    feat: improve checkout page

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -1,38 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BaseLayout from "components/BaseLayout";
-import { useCart } from "react-use-cart";
+import { useCart, Item } from "react-use-cart";
 import { Typography } from "@mui/material";
 import { ShoppingCartItem } from "types";
 
 const CheckoutPage = () => {
   const { isEmpty, items, updateItemQuantity, removeItem } = useCart();
+  const [itemsInCart, setItemsInCart] = useState<Array<Item>>([]);
 
-  console.log(items);
-  if (isEmpty)
+  useEffect(() => {
+    setItemsInCart(items);
+  }, [items]);
+
+  if (itemsInCart.length < 1) {
     return (
       <BaseLayout>
         <p>Your cart is empty</p>
       </BaseLayout>
     );
+  }
 
   return (
     <BaseLayout>
       <Typography variant="h4">Checkout</Typography>
       <ul>
-        {items.map((item: ShoppingCartItem) => (
-          <li key={item.id}>
-            {item.quantity} x {item.name} &mdash;
-            <button
-              onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
-            >
-              -
-            </button>
-            <button
-              onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
-            >
-              +
-            </button>
-            <button onClick={() => removeItem(item.id)}>&times;</button>
+        {itemsInCart.map(({ quantity, name, id }) => (
+          <li key={id}>
+            <p>
+              {quantity} x {name}: {id}
+            </p>
+
+            <button onClick={() => removeItem(id)}>&times;</button>
           </li>
         ))}
       </ul>
