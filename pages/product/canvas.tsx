d46e7d1dec10f0714ac3599078214e7b60a7e867
JeroenDerks <jeroen.derks@lau.do>
2022-07-12 22:27:25 +0200

    feat: improve theme switching

--- pages/product/canvas.tsx
+++ pages/product/canvas.tsx
@@ -1,36 +1,76 @@
 import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import { themeOptions } from "components/ThemeSelector";
-import { EditorTheme } from "types";
+import type { ColoredDataSet, EditorTheme, LanguageOption } from "types";
+import { convertTextToArtworkColors } from "utils/convertTextToArtworkColors";
 import { getHighlighter, setCDN, Highlighter } from "shiki";
-import ProductPage from "components/ProductPage/ProductPage";
+import { intialEditorValue } from "utils/intialEditorValue";
+import { languageOptions } from "components/LanguageSelector/LanguageSelector";
+import { themeOptions } from "components/ThemeSelector";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import InputPane from "components/Panes/InputPane";
+import OutputPane from "components/Panes/OutputPane";
 setCDN("https://unpkg.com/shiki/");
 
 const Canvas = () => {
-  const [theme, setTheme] = useState<EditorTheme>(themeOptions[3]);
+  const [language, setLanguage] = useState<LanguageOption>(languageOptions[14]);
+  const [rawData, setRawData] = useState<ColoredDataSet>([[]]);
   const [shiki, setShiki] = useState<Highlighter | null>(null);
-  const [renderCount, setRenderCount] = useState<number>(1);
+  const [sketchRenewKey, setSketchRenewKey] = useState<number>(1);
+  const [theme, setTheme] = useState<EditorTheme>(themeOptions[3]);
+  const [userValue, setUserValue] = useState<string>(intialEditorValue);
+
+  const regenerateArtWork = () => {
+    setRawData(convertTextToArtworkColors());
+    setSketchRenewKey((c) => c + 1);
+  };
+
+  const handleInputTextChange = (inputValue: string) => {
+    setUserValue(inputValue);
+    regenerateArtWork();
+  };
 
   useEffect(() => {
-    setShiki(null);
-
+    if (!theme) return;
     getHighlighter({
       theme: theme.code,
-    }).then((highlighter: Highlighter) => {
-      setShiki(highlighter);
-      setRenderCount((c) => c + 1);
+      langs: languageOptions.map(({ code }) => code),
+    }).then((hl: Highlighter) => {
+      setShiki(hl);
     });
   }, [theme]);
 
+  // regenrate art work every time shiki gets updated
+  useEffect(() => {
+    const shikiTheme = shiki?.getTheme();
+    if (shikiTheme && shikiTheme.name === theme.code) {
+      regenerateArtWork();
+    }
+  }, [shiki]);
+
   return (
     <Box p={5} width={1} maxWidth={1600} m="auto">
-      <ProductPage
-        renderCount={renderCount}
-        setRenderCount={(v) => setRenderCount(v)}
-        setTheme={setTheme}
-        shiki={shiki}
-        theme={theme}
-      />
+      <Grid container spacing={2}>
+        <Grid item sm={12} md={6}>
+          <InputPane
+            language={language}
+            setLanguage={setLanguage}
+            setTheme={setTheme}
+            setUserValue={handleInputTextChange}
+            shiki={shiki}
+            theme={theme}
+            userValue={userValue}
+          />
+        </Grid>
+        <Grid item sm={12} md={6}>
+          <OutputPane
+            language={language}
+            rawData={rawData}
+            sketchRenewKey={sketchRenewKey}
+            theme={theme}
+            userValue={userValue}
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
