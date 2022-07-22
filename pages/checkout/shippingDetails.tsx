0555ebb764e347999a630dfd852dadb540ef0933
JeroenDerks <jeroen.derks@lau.do>
2022-07-22 20:26:19 +0200

    fix: build errors

--- pages/checkout/shippingDetails.tsx
+++ pages/checkout/shippingDetails.tsx
@@ -3,8 +3,7 @@ import { useRouter } from "next/router";
 
 import Typography from "@mui/material/Typography";
 import ShippingAddress from "components/ShippingAddress";
-import BaseLayout from "components/BaseLayout";
-import BorderBox from "components/BorderBox";
+import Section from "components/Section";
 
 import type { ShippingAddressValues } from "types";
 
@@ -17,18 +16,16 @@ const ShippingDetails = () => {
   };
 
   return (
-    <BaseLayout>
-      <BorderBox bx mb={5}>
-        <Typography variant="h5">Shipping details</Typography>
-        <Typography variant="subtitle1" mb={4}>
-          Provide your address details, and we will provide the best shipping
-          prices.
-        </Typography>
-        <ShippingAddress
-          onSubmit={(v: ShippingAddressValues) => saveShippingDetails(v)}
-        />
-      </BorderBox>
-    </BaseLayout>
+    <Section>
+      <Typography variant="h5">Shipping details</Typography>
+      <Typography variant="subtitle1" mb={4}>
+        Provide your address details, and we will provide the best shipping
+        prices.
+      </Typography>
+      <ShippingAddress
+        onSubmit={(v: ShippingAddressValues) => saveShippingDetails(v)}
+      />
+    </Section>
   );
 };
 
