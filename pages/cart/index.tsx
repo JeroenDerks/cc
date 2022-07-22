04b2412cc98c15ee15f2bc376c9739b2e10778e9
JeroenDerks <jeroen.derks@lau.do>
2022-07-22 20:26:19 +0200

    fix: build errors

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material";
-import BaseLayout from "components/BaseLayout";
 import { useCart, Item } from "react-use-cart";
 import { Box, Button, Grid, Stack, Typography } from "@mui/material";
-import BorderBox from "components/BorderBox";
 import Flex from "components/Flex";
 import SeparatorLine from "components/SeparatorLine";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useRouter } from "next/router";
+import Section from "components/Section";
 
 const Image = styled("img")({
   width: "100%",
@@ -25,14 +24,12 @@ const CartPage = () => {
 
   if (itemsInCart.length < 1) {
     return (
-      <BaseLayout>
-        <BorderBox bx>
-          <Typography variant="h4">Cart</Typography>
-          <Typography variant="subtitle1" gutterBottom>
-            Your cart is empty
-          </Typography>
-        </BorderBox>
-      </BaseLayout>
+      <Section>
+        <Typography variant="h4">Cart</Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          Your cart is empty
+        </Typography>
+      </Section>
     );
   }
 
@@ -56,65 +53,61 @@ const CartPage = () => {
   };
 
   return (
-    <BaseLayout>
-      <BorderBox bx>
-        <Typography variant="h4">Cart</Typography>
-        <SeparatorLine />
-        <Stack>
-          {itemsInCart.map(({ quantity, name, id, price }) => (
-            <Grid container spacing={4} key={id}>
-              <Grid item xs={12} sm={12} md={6}>
-                <Image
-                  src={`https://storage.googleapis.com/highlight_images/${id}_preview.jpg`}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} md={6}>
-                <Flex
-                  height="100%"
-                  flexDirection="column"
-                  justifyContent="space-between"
-                >
-                  <Box>
-                    <Typography variant="h5">
-                      {quantity} x {name}
-                    </Typography>
-                    <Typography variant="body2">{id}</Typography>
-                  </Box>
-                  <Flex mt={3} justifyContent="space-between" width={1}>
-                    <Button
-                      aria-label="delete item"
-                      color="error"
-                      variant="outlined"
-                      size="small"
-                      startIcon={<DeleteIcon fontSize="small" />}
-                      onClick={() => removeItem(id)}
-                    >
-                      Delete
-                    </Button>
-                    <Typography variant="h6">
-                      Price: {price * 0.01},00
-                    </Typography>
-                  </Flex>
-                </Flex>
-              </Grid>
-              <SeparatorLine />
+    <>
+      <Typography variant="h4">Cart</Typography>
+      <SeparatorLine />
+      <Stack>
+        {itemsInCart.map(({ quantity, name, id, price }) => (
+          <Grid container spacing={4} key={id}>
+            <Grid item xs={12} sm={12} md={6}>
+              <Image
+                src={`https://storage.googleapis.com/highlight_images/${id}_preview.jpg`}
+              />
             </Grid>
-          ))}
-        </Stack>
-        <Flex justifyContent="space-between" alignItems="center">
-          <Typography variant="h5">Total: {sum * 0.01},00</Typography>
-          <Button
-            onClick={handleCheckout}
-            variant="outlined"
-            color="success"
-            size="large"
-            disabled={loading}
-          >
-            Checkout
-          </Button>
-        </Flex>
-      </BorderBox>
-    </BaseLayout>
+            <Grid item xs={12} sm={12} md={6}>
+              <Flex
+                height="100%"
+                flexDirection="column"
+                justifyContent="space-between"
+              >
+                <Box>
+                  <Typography variant="h5">
+                    {quantity} x {name}
+                  </Typography>
+                  <Typography variant="body2">{id}</Typography>
+                </Box>
+                <Flex mt={3} justifyContent="space-between" width={1}>
+                  <Button
+                    aria-label="delete item"
+                    color="error"
+                    variant="outlined"
+                    size="small"
+                    startIcon={<DeleteIcon fontSize="small" />}
+                    onClick={() => removeItem(id)}
+                  >
+                    Delete
+                  </Button>
+                  <Typography variant="h6">Price: {price * 0.01},00</Typography>
+                </Flex>
+              </Flex>
+            </Grid>
+            <SeparatorLine />
+          </Grid>
+        ))}
+      </Stack>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Typography variant="h5">Total: {sum * 0.01},00</Typography>
+        <Button
+          onClick={handleCheckout}
+          variant="outlined"
+          color="success"
+          size="large"
+          disabled={loading}
+        >
+          Checkout
+        </Button>
+      </Flex>
+    </>
   );
 };
 
