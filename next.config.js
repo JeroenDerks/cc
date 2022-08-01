629b3b955bf517b7c4d2f939d9603880135d8cd6
JeroenDerks <jeroen.derks@lau.do>
2022-08-01 13:11:14 +0200

    feat: add environment variables

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   ignoreBuildErrors: true,
   env: {
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+    BUCKET_NAME: process.env.BUCKET_NAME,
+    PROJECT_ID: process.env.PROJECT_ID,
   },
 };
 
