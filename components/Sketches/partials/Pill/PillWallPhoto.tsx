6b26d643a723e6daf7bdc1bb820f3800542ee101
JeroenDerks <jeroen.derks@lau.do>
2022-08-23 21:53:14 +0200

    feat: add pill sketches

---
+++ components/Sketches/partials/Pill/PillWallPhoto.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import dynamic from "next/dynamic";
+import p5Types from "p5";
+import Sketch from "react-p5";
+import { groupDataByColor } from "utils";
+import { sketchWidth, sketchHeigth, wallPhotoDimensions } from "../constants";
+import { SketchProps } from "types";
+import { drawPillContent } from "./drawPillContent";
+
+const BasiPillWallPhotoSketch = dynamic(
+  () => import("react-p5").then((mod) => mod.default as typeof Sketch),
+  { ssr: false }
+) as typeof Sketch;
+
+let backgroundImg: p5Types.Image;
+
+const PillWallPhoto: React.FC<SketchProps> = ({ bg, data }: SketchProps) => {
+  const groupedData = groupDataByColor(data);
+
+  const preload = (p5: p5Types) => {
+    p5.loadImage("/images/hero-bg-small.jpg", (img) => {
+      backgroundImg = img;
+    });
+  };
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(sketchWidth, sketchHeigth).parent(canvasParentRef);
+    p5.background(bg[0], bg[1], bg[2]);
+    p5.noStroke();
+
+    const graphic = drawPillContent({ groupedData, bg, p5, sx: 1, sy: 1 });
+    p5.image(backgroundImg, 0, 0);
+    const { x, y, w, h } = wallPhotoDimensions;
+    p5.fill(0, 0, 0, 50);
+    p5.rect(x + 4, y + 4, w, h, 2);
+    p5.rect(x + 2, y + 2, w, h, 2);
+    p5.rect(x + 1, y + 1, w, h, 1);
+    p5.image(graphic, x, y, w, h);
+  };
+
+  return <BasiPillWallPhotoSketch setup={setup} preload={preload} />;
+};
+
+export default PillWallPhoto;
