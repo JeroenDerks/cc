800f3ec9123362c74f7171827069cf3679ab3065
JeroenDerks <jeroen.derks@lau.do>
2022-08-24 10:57:16 +0200

    feat: remove scaling

--- components/ProductSwiper/ProductSwiper.tsx
+++ components/ProductSwiper/ProductSwiper.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, Autoplay } from "swiper";
 import { convertColorToRGB } from "utils";
-import Box from "@mui/material/Box";
 import type { ColoredDataSet, EditorTheme, Sketch } from "types";
 
 import "swiper/css";
@@ -31,28 +30,26 @@ const OutputPane = ({
   const selectedSketch = sketchOptions[parseInt(sketchId)];
 
   return (
-    <Box sx={{ transform: { sm: "scale(0.5)" } }}>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-        navigation={{ nextEl: ".product-next", prevEl: ".product-prev" }}
-        loop
-        autoplay={{ disableOnInteraction: true, delay: 6000 }}
-      >
-        {selectedSketch.sketches.map((Sketch, i) => (
-          <SwiperSlide>
-            <Sketch
-              bg={convertColorToRGB(theme.bg)}
-              key={sketchRenewKey + i}
-              loading={loading}
-              data={rawData}
-              uuid={uuid}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Box>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+      navigation={{ nextEl: ".product-next", prevEl: ".product-prev" }}
+      loop
+      autoplay={{ disableOnInteraction: true, delay: 6000 }}
+    >
+      {selectedSketch.sketches.map((Sketch, i) => (
+        <SwiperSlide>
+          <Sketch
+            bg={convertColorToRGB(theme.bg)}
+            key={sketchRenewKey + i}
+            loading={loading}
+            data={rawData}
+            uuid={uuid}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
