faaa18ccab13c091b333753160189d7556150ae2
JeroenDerks <jeroen.derks@lau.do>
2022-09-06 20:11:30 +0200

    feat: add sendgrid api key

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@ const nextConfig = {
   reactStrictMode: false,
   ignoreBuildErrors: true,
   env: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    PRIVATE_KEY: process.env.PRIVATE_KEY,
-    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
     BUCKET_NAME: process.env.BUCKET_NAME,
+    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
     PROJECT_ID: process.env.PROJECT_ID,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
   },
 };
 
