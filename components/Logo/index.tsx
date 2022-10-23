53f35c07024092e09bce0f07a78d62241279cf33
JeroenDerks <jeroen.derks@lau.do>
2022-10-23 20:03:26 +0200

    feat: make logo bigger

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -4,15 +4,11 @@ import { styled, Typography } from "@mui/material";
 const LogoComponent = styled(Typography)(({ theme }) => ({
   color: theme.palette.text.primary,
   fontWeight: 600,
-  fontSize: 18,
+  fontSize: 20,
   lineHeight: 1,
   textDecoration: "none",
 }));
 
 export const Logo = ({ as }: { as?: "a" }) => (
-  <LogoComponent as={as}>
-    Celebrate
-    <br />
-    Code
-  </LogoComponent>
+  <LogoComponent as={as}>Celebrate Code</LogoComponent>
 );
