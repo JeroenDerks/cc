2e9a4db3d14638981b6f609eb26c3d77eb237474
JeroenDerks <jeroen.derks@lau.do>
2022-10-23 19:35:02 +0200

    feat: add language to html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <title>Celebrate your team's code with a custom art work</title>
 
