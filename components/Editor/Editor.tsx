33e5f44e5b10c54fcd810b192e32a76716e4c402
JeroenDerks <jeroen.derks@lau.do>
2022-10-23 19:22:17 +0200

    feat: add label to editor

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer, useRef } from "react";
 import { EditorWrapper } from "./Editor.styles";
 import { LanguageOption } from "types";
 import Editor from "react-simple-code-editor";
@@ -32,24 +32,40 @@ const InputEditor = ({
 }) => {
   const [textValue, setTextValue] = useState<string>(userValue || "");
   const debouncedUserValue = useDebounce(textValue, 500);
+  const editorRef = useRef(null);
 
   useEffect(() => {
     setUserValue(debouncedUserValue);
   }, [debouncedUserValue]);
 
+  useEffect(() => {
+    if (editorRef.current) {
+      const editor = document.getElementById("input_editor");
+    }
+  }, [editorRef]);
+
   return (
     <EditorWrapper bg={shiki?.getBackgroundColor()}>
       {shiki && (
-        <Editor
-          value={textValue}
-          onValueChange={(code) => setTextValue(code)}
-          highlight={(code) => shiki.codeToHtml(code, { lang: language.code })}
-          style={{
-            fontFamily: '"Fira code", "Fira Mono", monospace',
-            fontSize: window?.innerWidth < 800 ? 12 : 14,
-            minHeight: window?.innerWidth < 800 ? 200 : 493,
-          }}
-        />
+        <>
+          <label htmlFor="input_editor" style={{ display: "none" }}>
+            Input editor
+          </label>
+          <Editor
+            value={textValue}
+            ref={editorRef}
+            textareaId="input_editor"
+            onValueChange={(code) => setTextValue(code)}
+            highlight={(code) =>
+              shiki.codeToHtml(code, { lang: language.code })
+            }
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: window?.innerWidth < 800 ? 12 : 14,
+              minHeight: window?.innerWidth < 800 ? 200 : 493,
+            }}
+          />
+        </>
       )}
       {!shiki && "Just preparing the palette..."}
     </EditorWrapper>
