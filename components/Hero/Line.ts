f6309974224b91b3d44fe4cc2761a579850ba529
Jeroen Derks <jeroenderks88@gmail.com>
2024-11-14 19:06:53 +0100

    feat: ad typewriter effect

--- components/Hero/Line.ts
+++ components/Hero/Line.ts
@@ -8,6 +8,8 @@ export class TokenRow {
   private incr = 0;
   private opacityIncr = 1;
   public isActive = true;
+  private phase = 0;
+  private typeCount = 0;
 
   constructor(tokens: ShikiData[], xOff: number, yOff: number) {
     this.xOff = xOff;
@@ -23,18 +25,21 @@ export class TokenRow {
   }
 
   draw(p5: p5Types) {
-    if (this.incr < charW) this.incr += 0.1;
+    this.typeCount += 0.4;
+    if (this.incr < charW) this.incr += 0.02;
     else if (this.opacityIncr >= 0) this.opacityIncr -= 0.01;
     else this.isActive = false;
 
     p5.noStroke();
     this.letters.forEach(({ content, color }, index) => {
-      p5.fill(color[0], color[1], color[2], Math.round(150 * this.opacityIncr));
+      if (index > this.typeCount) return;
+
       const w = charW + this.incr;
       const x = this.xOff + w * index;
       const y = this.yOff;
 
-      p5.rect(x, y, w, charH);
+      // p5.fill(color[0], color[1], color[2], Math.round(220 * this.opacityIncr));
+      // p5.rect(x, y, w, charH);
 
       p5.fill(color[0], color[1], color[2], Math.round(255 * this.opacityIncr));
       p5.text(content, x, y + charHOffset, w, charH);
